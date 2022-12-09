ae3d32df51ca80e413dbc1c9b6ff75ec3dde991c
Adrian Lachowicz <adrianlachowicz36@gmail.com>
2022-12-09 09:32:47 +0100

    Added config file
    Added config file, which contains parameters for training like batch_size etc., paths to datasets directories, and labels dict.

---
+++ datasets.py
@@ -0,0 +1,42 @@
+import glob
+import torch
+from torch.utils.data import Dataset
+import torchvision.transforms as T
+from utils import get_img_data
+
+
+class UnoDataset(Dataset):
+    """
+    The Uno dataset.
+
+    Arguments:
+        train (bool) - Use dataset for training or validation.
+    """
+
+    def __init__(self, train: bool = True):
+        if train is True:
+            self.data_path = "./data/train/"
+        else:
+            self.data_path = "./data/valid/"
+
+        self.xml_paths = glob.glob(self.data_path + "*.xml")
+        self.transform = T.Compose([T.ToTensor()])
+
+    def __len__(self):
+        return len(self.xml_paths)
+
+    def __getitem__(self, item):
+        xml_path = self.xml_paths[item]
+
+        img, labels, boxes = get_img_data(xml_path, self.data_path)
+
+        img = self.transform(img)
+        labels = torch.tensor(labels, dtype=torch.int64)
+        boxes = torch.tensor(boxes, dtype=torch.float32)
+
+        target = {"boxes": boxes, "labels": labels}
+
+        return img, target
+
+    def collate_fn(self, batch):
+        return list(zip(*batch))
---
+++ utils.py
@@ -0,0 +1,43 @@
+from config import LABELS
+from PIL import Image
+from xml.etree import ElementTree as ET
+
+
+def get_img_data(xml_path: str, data_path: str):
+    """
+    The function extracts data from XML files.
+
+    Arguments:
+        xml_path (str) - Path to a XML file.
+        data_path (str) - Path to a specific data directory.
+
+    Returns:
+        img (PIL.Image) - A loaded image based on path from XML file.
+        labels  (list) - List with labels of bounding boxes.
+        boxes (list) - List with bounding boxes coordinates in Pascal_VOC format.
+    """
+
+    root = ET.parse(xml_path).getroot()
+
+    img_filename = root.find("filename").text
+
+    img = Image.open(data_path + img_filename)
+    labels = []
+    boxes = []
+
+    objects = root.findall("object")
+
+    for object_ in objects:
+        label = object_.find("name").text
+        label = LABELS[label]
+
+        bnd_box = object_.find("bndbox")
+        xmin = int(bnd_box.find("xmin").text)
+        ymin = int(bnd_box.find("ymin").text)
+        xmax = int(bnd_box.find("xmax").text)
+        ymax = int(bnd_box.find("ymax").text)
+
+        labels.append(label)
+        boxes.append([xmin, ymin, xmax, ymax])
+
+    return img, labels, boxes
