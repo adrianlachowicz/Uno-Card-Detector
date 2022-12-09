b37de8548a450dac633319715584e4f632fb4bf9
Adrian Lachowicz <adrianlachowicz36@gmail.com>
2022-12-09 09:38:15 +0100

    Added config file
    Added config file, which contains parameters for training like batch_size etc., paths to datasets directories, and labels dict.

---
+++ config.py
@@ -0,0 +1,35 @@
+import torch
+
+BATCH_SIZE = 8
+IMG_SIZE = 256
+NUM_EPOCHS = 10
+NUM_WORKERS = 4
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+TRAIN_DIR = "data/train/"
+VAL_DIR = "data/valid/"
+TEST_DIR = "data/test/"
+
+LABELS = {
+    "background": 0,
+    "0": 1,
+    "1": 2,
+    "2": 3,
+    "3": 4,
+    "4": 5,
+    "5": 6,
+    "6": 7,
+    "7": 8,
+    "8": 9,
+    "9": 10,
+    "10": 11,  # +4 card
+    "11": 12,  # +2 card
+    "12": 13,  # Double arrow card
+    "13": 14,  # Phi card
+    "14": 15,  # Wildcard (colored circle)
+}
+
+NUM_LABELS = len(LABELS)
+
+OUT_DIR = "./outputs"
